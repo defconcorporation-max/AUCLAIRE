010995d5ed218a4ac0d84c542cb395232cc7402d
tako <dorian2992@hotmail.fr>
2026-02-09 11:47:43 -0500

    Build: Force Retry Deployment V2.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// Force new deployment: V2.1 Fixed Affiliates
-console.log("App Version: V2.1 - Fixed Affiliates & Profit Logic");
+// Force new deployment: V2.3 Fixed Affiliates (Retry)
+console.log("App Version: V2.3 - Fixed Affiliates & Profit Logic");
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import CRMLayout from './components/layout/CRMLayout';
